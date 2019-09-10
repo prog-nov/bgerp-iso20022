6b73302179868142fe77ddd0deafdd38382f9f35
Ivelin Dimov <ivelin_pdimov@abv.bg>
2019-09-10 15:33:41 +0300

    Протокол за производство да не се начислявят режийни разходи на етап

--- planning/DirectProductionNote.class.php
+++ planning/DirectProductionNote.class.php
@@ -291,9 +291,14 @@ class planning_DirectProductionNote extends planning_ProductionDocument
                 $form->setField('packagingId', 'input');
             }
             
-            $bomRec = cat_Products::getLastActiveBom($rec->productId, 'production,sales');
-            if (isset($bomRec->expenses)) {
-                $form->setDefault('expenses', $bomRec->expenses);
+            // Ако се произвежда етап, не може да се начисляват режийни разходи
+            if($rec->productId != $jobRec->productId && cat_Products::haveDriver($rec->productId, 'planning_interface_StageDriver')){
+                $form->setField('expenses', 'input=none');
+            } else {
+                $bomRec = cat_Products::getLastActiveBom($rec->productId, 'production,sales');
+                if (isset($bomRec->expenses)) {
+                    $form->setDefault('expenses', $bomRec->expenses);
+                }
             }
         }
         
